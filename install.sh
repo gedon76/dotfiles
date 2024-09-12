39cd1a178c044a37e7cb4a53f717653ac33c5745
Gedon76 <82772622+gedon76@users.noreply.github.com>
2024-09-12 22:23:13 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 # if you're lazy to just copy everything yourself...
 cp ./.config/* ~/.config/ -r
 cp ./wallpapers ~ -r
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+# installing vim-plug
 home-manager switch
