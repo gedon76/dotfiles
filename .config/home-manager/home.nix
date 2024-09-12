c54b15a49d2404bcec3dd51cf2907208adfc859e
Gedon76 <82772622+gedon76@users.noreply.github.com>
2024-09-12 20:56:37 +0300

    Update home.nix

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -3,8 +3,8 @@
 {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
-  home.username = "gedon76";
-  home.homeDirectory = "/home/gedon76";
+  home.username = "yourusername";
+  home.homeDirectory = "/home/yourusernamr";
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
